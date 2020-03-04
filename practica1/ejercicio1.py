bda872bf5649c8845101822770ed09e3dda8f7ba
David Cabezas Berrido <dxabezas@gmail.com>
2020-03-04 13:41:35 +0100

    Enunciados practicas 0 y 1. Empezado el 1b de la practica 1

--- practica1/ejercicio1.py
+++ practica1/ejercicio1.py
@@ -6,7 +6,7 @@ import sympy as sp
 
 u, v = sp.symbols('u v')
 
-# Función de error (a minimizar)
+# Función a minimizar
 E_sym=sp.Lambda((u,v), sp.simplify((u*sp.exp(v)-2*v*sp.exp(-u))**2))
 def E(w):
     return float(E_sym(w[0],w[1]))
@@ -25,6 +25,7 @@ def Ev(w):
 def gradE(w):
 	return np.array((Eu(w), Ev(w)))
 
+# Algoritmo del gradiente descendente
 def gd(w, lr, grad_fun, fun, epsilon, max_iters = 10000):
 
 	it = 0
@@ -32,15 +33,15 @@ def gd(w, lr, grad_fun, fun, epsilon, max_iters = 10000):
 	while it < max_iters and fun(w)>epsilon:
 		it+=1
 		grad=grad_fun(w)
-		w=w-lr*grad#/np.linalg.norm(grad)
-		#print(w, fun(w))
+		w=w-lr*grad
+		# print(w, fun(w))
 
 	return w, it
 
 # Punto inicial
 w=np.array([1,1])
 # Número máximo de iteraciones
-max_iters=5000
+max_iters=500
 # Learning rate
 lr=0.1
 # Margen de error
@@ -55,3 +56,26 @@ input("\n--- Pulsar Intro para continuar ---\n")
 print ('Coordenadas obtenidas: (', w[0], ', ', w[1],')')
 
 input("\n--- Pulsar Intro para continuar ---\n")
+
+#------------------------------Ejercicio 2 -------------------------------------#
+
+x, y = sp.symbols('x y')
+
+# Función a minimizar
+f_sym=sp.Lambda((x,y), sp.simplify((x-2)**2+2*(y+2)**2+2*sp.sin(2*sp.pi*x)*sp.sin(2*sp.pi*y)))
+def f(w):
+	return float(f_sym(w[0],w[1]))
+	
+# Derivada parcial de f respecto de x
+fx_sym=sp.Lambda((x,y), sp.simplify(sp.diff(f_sym(x,y),x)))
+def fx(w):
+	return float(fx_sym(w[0],w[1]))
+
+# Derivada parcial de f respecto de y
+fy_sym=sp.Lambda((x,y), sp.simplify(sp.diff(f_sym(x,y),y)))
+def fy(w):
+	return float(fy_sym(w[0],w[1]))
+	
+# Gradiente de f
+def gradf(w):
+	return np.array([fx(w), fy(w)])
