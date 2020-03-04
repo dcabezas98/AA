39cf62958d0c0d31cfb8cf11bcd9770fb7e7a2f3
David Cabezas Berrido <dxabezas@gmail.com>
2020-03-04 22:16:28 +0100

    P1, ejer1 completado

--- practica1/ejercicio1.py
+++ practica1/ejercicio1.py
@@ -1,8 +1,18 @@
+# -*- coding: utf-8 -*-
+
 # Práctica 1, Ejercicio 1
 # David Cabezas Berrido
 
+#-------------------------------------------------------------------------------#
+#------------- Ejercicio sobre la búsqueda iterativa de óptimos ----------------#
+#-------------------------------------------------------------------------------#
+
+
+#------------------------------Ejercicio 1 -------------------------------------#
+
 import numpy as np
 import sympy as sp
+import matplotlib.pyplot as plt
 
 u, v = sp.symbols('u v')
 
@@ -26,14 +36,14 @@ def gradE(w):
 	return np.array((Eu(w), Ev(w)))
 
 # Algoritmo del gradiente descendente
-def gd(w, lr, grad_fun, fun, epsilon, max_iters = 10000):
+def gd(w, lr, grad_fun, fun, epsilon, max_iters = 1000):
 
 	it = 0
 
 	while it < max_iters and fun(w)>epsilon:
 		it+=1
-		grad=grad_fun(w)
-		w=w-lr*grad
+		grad=grad_fun(w) # Calcula gradiente
+		w=w-lr*grad      # Actualiza el punto
 		# print(w, fun(w))
 
 	return w, it
@@ -41,7 +51,7 @@ def gd(w, lr, grad_fun, fun, epsilon, max_iters = 10000):
 # Punto inicial
 w=np.array([1,1])
 # Número máximo de iteraciones
-max_iters=500
+max_iters=100
 # Learning rate
 lr=0.1
 # Margen de error
@@ -56,6 +66,7 @@ input("\n--- Pulsar Intro para continuar ---\n")
 print ('Coordenadas obtenidas: (', w[0], ', ', w[1],')')
 
 input("\n--- Pulsar Intro para continuar ---\n")
+
 
 #------------------------------Ejercicio 2 -------------------------------------#
 
@@ -78,4 +89,84 @@ def fy(w):
 	
 # Gradiente de f
 def gradf(w):
-	return np.array([fx(w), fy(w)])
+	return np.array((fx(w), fy(w)))
+
+# a) Usar gradiente descendente para minimizar la función f, con punto inicial (1,-1)
+# tasa de aprendizaje 0.01 y max 50 iteraciones. Repetir con tasa de aprend. 0.1
+
+def gd_grafica(w, lr, grad_fun, fun, max_iters = 1000):
+
+	graf = []
+
+	for _ in range(max_iters):
+		grad=grad_fun(w)
+		w=w-lr*grad
+		graf.append(fun(w))
+		#print(w, fun(w))
+
+	graf = np.array(graf)
+
+	plt.plot(range(1,max_iters+1), graf, 'bo')
+	plt.xlabel('Iteraciones')
+	plt.ylabel('f(x,y)')
+	plt.show()	
+
+
+# Punto inicial
+w=np.array([1,-1])
+# Número máximo de iteraciones
+max_iters=50
+
+print ('Resultados ejercicio 2\n')
+print ('\nGrafica con learning rate igual a 0.01')
+lr=0.01 # Learning rate
+gd_grafica(w,lr,gradf,f,max_iters)
+print ('\nGrafica con learning rate igual a 0.1')
+lr=0.1 # Learning rate
+gd_grafica(w,lr,gradf,f,max_iters)
+input("\n--- Pulsar tecla para continuar ---\n")
+
+
+# b) Obtener el valor minimo y los valores de (x,y) con los
+# puntos de inicio siguientes:
+
+def gd(w, lr, grad_fun, fun, max_iters = 1000):
+	
+	for _ in range(max_iters):
+		grad=grad_fun(w)
+		w=w-lr*grad
+		#print(w, fun(w))
+	
+	return w
+
+lr=0.01 # Learning rate (no se especifica)
+# Número máximo de iteraciones (tampoco se especifica)
+max_iters=50
+
+print ('Punto de inicio: (2.1, -2.1)\n')
+w=gd(np.array((2.1,-2.1)),lr,gradf,f,max_iters)
+print ('(x,y) = (', w[0], ', ', w[1],')\n')
+print ('Valor minimo: ',f(w))
+
+input("\n--- Pulsar tecla para continuar ---\n")
+
+print ('Punto de inicio: (3.0, -3.0)\n')
+w=gd(np.array((3.0,-3.0)),lr,gradf,f,max_iters)
+print ('(x,y) = (', w[0], ', ', w[1],')\n')
+print ('Valor minimo: ',f(w))
+
+input("\n--- Pulsar tecla para continuar ---\n")
+
+print ('Punto de inicio: (1.5, 1.5)\n')
+w=gd(np.array((1.5,1.5)),lr,gradf,f,max_iters)
+print ('(x,y) = (', w[0], ', ', w[1],')\n')
+print ('Valor minimo: ',f(w))
+
+input("\n--- Pulsar tecla para continuar ---\n")
+
+print ('Punto de inicio: (1.0, -1.0)\n')
+w=gd(np.array((1.0,-1.0)),lr,gradf,f,max_iters)
+print ('(x,y) = (', w[0], ', ', w[1],')\n')
+print ('Valor mínimo: ',f(w))
+
+input("\n--- Pulsar tecla para continuar ---\n")
